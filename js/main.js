401c99d6f5cf5f90287558f1e2c62ef200dcc805
Calvin <calvin.chan.h@gmail.com>
2014-11-10 01:00:33 -0800

    v 0.0.2

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ if (username) {
     document.getElementById('sendLink').addEventListener('click', sendLink);
     document.getElementById('sendClip').addEventListener('click', sendClip);
 } else {
-    document.getElementById("message").innerHTML = "It seems like you haven't set your YO username yet. Please configure YOMarq with your YO username under the Chrome Extensions settings.";
+    document.getElementById("message").innerHTML = "It seems like you haven't set your Yo username yet. Please configure YoClip with your Yo username under Chrome Extensions settings.";
 }
 
 function sendLink() {
@@ -14,12 +14,13 @@ function sendLink() {
             active: true
         }, function(tab) {
             if (tab.length > 0) {
-                $.post('http://localhost:1337/yo', {
+                $.post('http://yoclip.herokuapp.com/yo', {
                     username: settings.get('username'),
                     link: tab[0].url
                 }, function(data) {
-                    console.log(data);
+                    data = JSON.parse(data);
                     if (data.result === "OK") {
+                        console.log("suckacock");
                         document.getElementById("message").innerHTML = "Your link has been sent to "+settings.get('username') ;
                     }
                 });
@@ -34,16 +35,30 @@ function sendClip() {
     var clip = '',
         input = $('#clip').val('').select();
     if(document.execCommand('paste')) {
+        console.log(input.val());
         clip = input.val();
     }
-    input.val('');
-    $.post('http://localhost:1337/yo/clip', {
+    console.log(clip);
+    clip = getClipboard();
+    //input.val('');
+    $.post('http://yoclip.herokuapp.com/yo/clip', {
         username: settings.get('username'),
         clip: clip
     }, function(data) {
-        console.log(data);
+        data = JSON.parse(data);
         if (data.result === "OK") {
             document.getElementById("message").innerHTML = "Your clipboard has been sent to "+settings.get('username') ;
         }
     });
 }
+
+function getClipboard() {
+    var pasteTarget = document.createElement("div");
+    pasteTarget.contentEditable = true;
+    var actElem = document.activeElement.appendChild(pasteTarget).parentNode;
+    pasteTarget.focus();
+    document.execCommand("Paste", null, null);
+    var paste = pasteTarget.innerText;
+    actElem.removeChild(pasteTarget);
+    return paste;
+};
